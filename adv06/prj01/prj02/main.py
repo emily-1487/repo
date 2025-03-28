9a402850472da243ecca0e7b55c597c7f61b6dd8
emily-1487 <emilyliu11022011@gmail.com>
2025-03-28 20:57:42 +0800

    adv06

---
+++ adv06/prj01/prj02/main.py
@@ -0,0 +1,13 @@
+#######################載入模組########################
+from machine import Pin, ADC
+from time import sleep
+import mcu
+
+#######################宣告與設定########################
+gpio = mcu.gpio()
+light_sensor = ADC(0)
+#######################主程式########################
+while True:
+    light_sensor_reading = light_sensor.read()
+    print(f"value:{light_sensor_reading},{round(light_sensor_reading*100/1024)}%")
+    sleep(1)
